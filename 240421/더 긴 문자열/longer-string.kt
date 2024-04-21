100f5ece8cd161b6fb9fa5904e07ebf627c06044
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2024-04-21 21:31:53 +0900

    Title: 더 긴 문자열, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240421/더 긴 문자열/longer-string.kt
@@ -0,0 +1,27 @@
+import java.io.*
+import java.math.RoundingMode
+import java.text.DecimalFormat
+import java.util.Arrays
+import java.util.Deque
+import java.util.LinkedList
+import java.util.Queue
+import java.util.Scanner
+import java.util.Stack
+import java.util.StringTokenizer
+import java.util.TreeMap
+import java.util.regex.Matcher
+import java.util.regex.Pattern
+import kotlin.math.*
+
+fun main45345() {
+    val br = System.`in`.bufferedReader()
+    val (n, k) = br.readLine().split(" ").map { it }
+    
+    if (n.length > k.length) {
+        println("$n ${n.length}")
+    } else if (n.length < k.length) {
+        println("$k ${k.length}")
+    } else {
+        println("same")
+    }
+}
