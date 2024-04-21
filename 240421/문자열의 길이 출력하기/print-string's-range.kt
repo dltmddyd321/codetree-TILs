c6e2d7cd177cc4d66f4eeae94622b5ecfc852a8b
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2024-04-21 21:23:31 +0900

    Title: 문자열의 길이 출력하기, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240421/문자열의 길이 출력하기/print-string's-range.kt
@@ -0,0 +1,8 @@
+import java.util.Scanner
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    val input = scanner.nextLine()
+    val second = scanner.nextLine()
+    println(input.length + second.length)
+}
