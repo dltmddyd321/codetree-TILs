1520835273ba654e4b339ea31d26ee8d313611ad
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2024-04-25 19:38:48 +0900

    Title: 지그재그로 숫자 채우기, Time: 279ms, Memory: 12MB, Status: Passed - CodeTree

--- 240425/지그재그로 숫자 채우기/zigzag-numbering.kt
+++ 240425/지그재그로 숫자 채우기/zigzag-numbering.kt
@@ -11,21 +11,35 @@ import java.util.regex.Matcher
 import java.util.regex.Pattern
 import kotlin.math.*
 
-fun main() {
-    fun printRectangle(width: Int, height: Int) {
-        // 각 행에 대해서
-        for (i in 0 until height) {
-            var start = i
-            // 각 열에 대해서
-            for (j in 0 until width) {
-                print("$start ")
-                start += height  // 다음 열의 숫자는 현재 행의 높이만큼 증가
+fun main(args: Array<String>) {
+        val sc = Scanner(System.`in`)
+
+        // n, m을 입력받습니다.
+        val n = sc.nextInt()
+        val m = sc.nextInt()
+
+        // 2차원 배열을 구현합니다.
+        val arr = Array(100) { IntArray(100) }
+
+        var count = 0
+        for (col in 0 until m) {
+            if (col % 2 == 0) {
+                for (row in 0 until n) {
+                    arr[row][col] = count
+                    count++
+                }
+            } else {
+                for (row in n - 1 downTo 0) {
+                    arr[row][col] = count
+                    count++
+                }
             }
-            println() // 각 행이 끝날 때마다 줄바꿈
+        }
+
+        for (row in 0 until n) {
+            for (col in 0 until m) {
+                print(arr[row][col].toString() + " ")
+            }
+            println()
         }
     }
-    val scanner = Scanner(System.`in`)
-    val height = scanner.nextInt()
-    val width = scanner.nextInt()
-    printRectangle(width, height)
-}
