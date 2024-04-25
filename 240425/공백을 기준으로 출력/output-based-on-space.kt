a51f251f203f4879711571d394cfd0892212e24f
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2024-04-25 19:50:24 +0900

    Title: 공백을 기준으로 출력, Time: 247ms, Memory: 18MB, Status: Passed - CodeTree

---
+++ 240425/공백을 기준으로 출력/output-based-on-space.kt
@@ -0,0 +1,25 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.Arrays
+import java.util.Deque
+import java.util.LinkedList
+import java.util.Queue
+import java.util.Scanner
+import java.util.Stack
+import java.util.StringTokenizer
+import java.util.TreeMap
+import java.util.regex.Matcher
+import java.util.regex.Pattern
+import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.sqrt
+
+fun main () {
+    val scanner = Scanner(System.`in`)
+    val input = scanner.nextLine().replace(" ", "")
+    val second = scanner.nextLine().replace(" ", "")
+    val str = input + second
+    println(str)
+}
