cf9e8a87d326aea08958884127611ff9e8cf8447
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2024-04-27 19:55:34 +0900

    Title: Run Length 인코딩, Time: 160ms, Memory: 12MB, Status: Wrong Answer - CodeTree

---
+++ 240427/Run Length 인코딩/run-length-encoding.kt
@@ -0,0 +1,44 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.Arrays
+import java.util.Deque
+import java.util.LinkedList
+import java.util.Queue
+import java.util.Scanner
+import java.util.Stack
+import java.util.StringTokenizer
+import java.util.TreeMap
+import java.util.regex.Matcher
+import java.util.regex.Pattern
+import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.sqrt
+
+fun main() {
+    fun compressString(input: String): String {
+        if (input.isEmpty()) return ""
+        val result = StringBuilder()
+
+        var currentChar = input.first()
+        var count = 1
+
+        for (i in 1 until input.length) {
+            if (input[i] == currentChar) {
+                count++
+            } else {
+                result.append("$currentChar$count")
+                currentChar = input[i]
+                count = 1
+            }
+        }
+        result.append("$currentChar$count")
+
+        return result.toString()
+    }
+
+    val scanner = Scanner(System.`in`)
+    val input = scanner.nextLine()
+    println(compressString(input))
+}
