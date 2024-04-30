6ab21288967c88a8c4a20bddc882d838013bb02c
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2024-04-30 19:40:19 +0900

    Title: 정수만 추출하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240430/정수만 추출하기/extract-only-integers.kt
@@ -0,0 +1,37 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.Arrays
+import java.util.Deque
+import java.util.LinkedList
+import java.util.Queue
+import java.util.Scanner
+import java.util.Stack
+import java.util.StringTokenizer
+import java.util.TreeMap
+import java.util.regex.Matcher
+import java.util.regex.Pattern
+import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.sqrt
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    val arr = mutableListOf<Int>()
+    var temp = ""
+    val (first, second) = scanner.nextLine().split(" ")
+    fun check(input: String) {
+        for (i in input) {
+            if (i.isDigit()) temp += i
+            else {
+                arr.add(temp.toInt())
+                temp = ""
+                return
+            }
+        }
+    }
+    check(first)
+    check(second)
+    println(arr[0] + arr[1])
+}
