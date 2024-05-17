a5162c3dcdb465f3e1b3a79a1d0e7b0ba98e5455
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2024-05-17 21:15:52 +0900

    Title: 연속부분수열일까, Time: 159ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 240517/연속부분수열일까/contiguous-array-or-not.kt
@@ -0,0 +1,30 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.Arrays
+import java.util.Deque
+import java.util.LinkedList
+import java.util.Queue
+import java.util.Scanner
+import java.util.Stack
+import java.util.StringTokenizer
+import java.util.TreeMap
+import java.util.regex.Matcher
+import java.util.regex.Pattern
+import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.sqrt
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    val case = scanner.nextLine()
+    val first = scanner.nextLine()
+    val second = scanner.nextLine()
+
+    if (first.contains(second)) {
+        println("Yes")
+    } else {
+        println("No")
+    }
+}
