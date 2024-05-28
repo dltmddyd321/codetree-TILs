16702dc7d42e8eab103fd8c31144a88c2dc35cdf
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2024-05-28 20:59:03 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240528/중복되지 않는 숫자 중 최대/max-of-unique-number.kt
@@ -0,0 +1,27 @@
+import java.util.Scanner
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    val n = scanner.nextInt()
+    val arr = mutableMapOf<Int, Int>()
+
+    repeat(n) {
+        val k = scanner.nextInt()
+        arr[k] = (arr[k] ?: 0) + 1
+    }
+
+    val keysWithValueOne = arr.filter { it.value == 1 }.keys.toList()
+
+    if (keysWithValueOne.isEmpty()) {
+        println(-1)
+        return
+    }
+
+    val maxKey = keysWithValueOne.maxOrNull()
+
+    println(if (keysWithValueOne.count { it == maxKey } == 1) {
+        maxKey ?: -1
+    } else {
+        -1
+    })
+}
