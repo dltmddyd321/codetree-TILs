7539dc3134b2d7043476ae8855876354632591b9
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2024-06-14 19:16:47 +0900

    Title: 문자열 순서 바꾸기, Time: 122ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240614/문자열 순서 바꾸기/change-order-of-strings.kt
@@ -0,0 +1,9 @@
+import java.util.Scanner
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    val first = scanner.nextLine()
+    val second = scanner.nextLine()
+
+    println("$second\n$first")
+}
