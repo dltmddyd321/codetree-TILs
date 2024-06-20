efae783ffe7fdc40d3ac61629b2c50dd3df9d73e
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2024-06-20 20:22:10 +0900

    Title: 369 게임, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240620/369 게임/369-game.kt
+++ 240620/369 게임/369-game.kt
@@ -6,7 +6,7 @@ fun main() {
     val str = StringBuilder()
     
     for (i in 1..a) {
-        if (i % 3 == 0) {
+        if (i % 3 == 0 || i.toString.contains("3") || i.toString.contains("6") || i.toString.contains("9")) {
             str.append("0 ")
         } else {
             str.append("$i ")
